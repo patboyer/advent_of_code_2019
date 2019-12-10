5559ba5e4921c32428779a45a3da564a01ba07b9
Patrick Boyer <patrick.boyer@msj.org>
2019-12-10 08:50:05 -0500

    added basic testing with Chai

--- day01.js
+++ day01.js
@@ -1,7 +1,8 @@
 "use strict";
 
-const R     = require("ramda");
-const Utils = require("./utils.js");
+const expect = require("chai").expect;
+const R      = require("ramda");
+const Utils  = require("./utils.js");
 
 const calculateFuelA = (mass) => {
   return Math.floor(mass / 3) - 2;
@@ -25,6 +26,16 @@ const solveB = R.pipe(
   R.map(calculateFuelB),
   R.sum
 );
+
+expect(calculateFuelA(12)).to.be.equal(2);
+expect(calculateFuelA(14)).to.be.equal(2);
+expect(calculateFuelA(1969)).to.be.equal(654);
+expect(calculateFuelA(100756)).to.be.equal(33583);
+
+expect(calculateFuelB(12)).to.be.equal(2);
+expect(calculateFuelB(14)).to.be.equal(2);
+expect(calculateFuelB(1969)).to.be.equal(966);
+expect(calculateFuelB(100756)).to.be.equal(50346);
 
 const input = Utils.readFileToIntegers("input/day01.txt");
 console.log(`Day 01 A: ${ solveA(input) }`);  //= 3295206
